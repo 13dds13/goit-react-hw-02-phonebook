4622fa59d3406b40cf9fba9f7ca8bf41f15d545f
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-09-29 19:56:50 +0300

    update contactsList component

--- src/components/contactsList/contactsAllOrFiltered/ContactsAllOrFiltered.js
+++ src/components/contactsList/contactsAllOrFiltered/ContactsAllOrFiltered.js
@@ -2,31 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ContactsAllOrFiltered = ({ filter, styles, contacts, deleteContact }) => {
+  const contactsToRender = (contacts) => {
+    if (!filter) return contacts;
+
+    const filteredContacts = contacts.filter((item) =>
+      item.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    return filteredContacts;
+  };
   return (
     <>
       {filter ? <p>Search results:</p> : <p>All contacts:</p>}
       <ul className={styles.listWrap}>
-        {contacts
-          .filter((item) =>
-            item.name.toLowerCase().includes(filter.toLowerCase())
-          )
-          .map((item) => (
-            <li className={styles.listItem} key={item.id}>
-              {" "}
-              <p className={styles.name}>
-                {item.name}:{" "}
-                <span className={styles.number}>{item.number}</span>
-              </p>
-              <button
-                className={styles.btn}
-                name={item.name}
-                type="button"
-                onClick={() => deleteContact(item.name)}
-              >
-                Delete
-              </button>
-            </li>
-          ))}
+        {contactsToRender(contacts).map((item) => (
+          <li className={styles.listItem} key={item.id}>
+            {" "}
+            <p className={styles.name}>
+              {item.name}: <span className={styles.number}>{item.number}</span>
+            </p>
+            <button
+              className={styles.btn}
+              name={item.name}
+              type="button"
+              onClick={() => deleteContact(item.name)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
       </ul>
     </>
   );
